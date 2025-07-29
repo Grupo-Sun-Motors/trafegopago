6ad2247f86c8ea807673798b8b93f4f88d3397f0
SunMotorsOficial <mktsunmotors@gmail.com>
2025-07-29 14:01:29 -0300

    Atualiza├º├úo autom├ítica 29/07/2025 14:01:29,91

--- push-github.bat
+++ push-github.bat
@@ -1,31 +1,30 @@
 @echo off
-cd /d "%~dp0"
+cd /d %~dp0
 
-echo ---------------------------------------
-echo  [ GIT PUSH - Projeto trafegopago ]
-echo ---------------------------------------
+echo.
+echo ------------------------------------------
+echo    [ GIT PUSH - Projeto trafegopago ]
+echo ------------------------------------------
 
-REM Configurações iniciais (se ainda não estiverem feitas)
-git config user.name "FOSSIO-FTG-MCK"
-git config user.email "desenvolvimento@fossio.com.br"
+REM Inicializa repositório e configura usuário (se necessário)
+git init
+git config user.name "SunMotorsOficial"
+git config user.email "mktsunmotors@gmail.com"
 
-REM Evita enviar o arquivo sensível
+REM Adiciona remote (caso ainda não exista)
+git remote remove origin >nul 2>&1
+git remote add origin https://github.com/SunMotorsOficial/trafegopago.git
+
+REM Ignora supabase-infos.txt
 echo supabase-infos.txt>>.gitignore
 
-REM Adiciona tudo, exceto o que estiver no .gitignore
+REM Adiciona e commita tudo, exceto o supabase-infos.txt
 git add .
+git reset supabase-infos.txt
+git commit -m "Atualização automática %date% %time%"
 
-REM Mensagem de commit automática com data/hora
-set hora=%time:~0,2%:%time:~3,2%
-set data=%date:~-4%-%date:~3,2%-%date:~0,2%
-git commit -m "Atualização automática %data% %hora%"
-
-REM Define o repositório remoto se não tiver
-git remote remove origin >nul 2>&1
-git remote add origin https://github.com/SunMotorsOficial/trafegopago
-
-REM Envia para o repositório principal
-git push -u origin main --force
+REM Faz push do branch local master para o remoto main
+git push -f origin master:main
 
 echo.
 echo Push realizado com sucesso!
